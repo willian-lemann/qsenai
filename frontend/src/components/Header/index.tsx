cf03dffcf677359275b13d5848d3b349b72fdd51
Fabiano Dutra <fmbdutra@gmail.com>
2020-06-30 21:00:53 -0300

    ajuste questionsNumberCard na header

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,20 +1,15 @@
 import React from 'react';
-import api from '../../service/api';
 
 import './index.css';
 
-import { FiUser } from 'react-icons/fi';
-
 import QuestionsNumberCard from '../QuestionsNumberCard';
 import AddQuestionButton from '../AddQuestionButton';
-
-import api from '../../service/api';
 
 const Header: React.FC = () => {
 
     return (
         <div className="header-container">
-            <QuestionsCard />
+            <QuestionsNumberCard />
             <AddQuestionButton primary={true}> Lançar Pergunta </AddQuestionButton>
         </div>
     );
