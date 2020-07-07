a486703e16491d43553973cc46dc27965407bccf
willian.rochagit config --global user.name <willian.rocha@viaflow.com.br>
2020-07-07 02:14:33 -0300

    final

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import socketio from 'socket.io';
+import http from 'http';
 import cors from 'cors';
 import routes from './routes';
 const port = process.env.PORT || 3333;
 
 const app = express();
+
 
 app.use(cors({
     exposedHeaders: 'x-total-count'
